0af54ad78979e38c68dc955c6731b7426eaec245
Artur Kasza <arturkasza98@gmail.com>
2019-07-09 10:38:15 +0200

    Initial commit. Exercises 1-3 done.

---
+++ src/main/java/pl/fis/artur/kasza/lbd/SurveysServlet.java
@@ -0,0 +1,56 @@
+package pl.fis.artur.kasza.lbd;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.stream.IntStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class SurveysServlet
+ */
+@WebServlet("/surveys")
+public class SurveysServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String university = request.getParameter("university");
+		String faculty = request.getParameter("faculty");
+		String degree = request.getParameter("degree");
+		ArrayList<String> answers = new ArrayList<String>();
+		for(int i = 0; i<3; i++) {
+			answers.add(request.getParameter("answer"+i));
+		}
+		
+		response.getWriter()
+		.append(String.format("Thanks %s %s for participation in our university survey.\n", firstName, lastName))
+		.append(String.format("Your answers:\n"
+				+ "University name: %s\n"
+				+ "Faculty name: %s\n"
+				+ "Degree: %s\n", university, faculty, degree))
+		.append(String.format("Quality of courses & teaching?: %s\n"
+				+ "Contact with teachers?: %s\n"
+				+ "Inclusion of work/practical experience?: %s", answers.get(0), answers.get(1), answers.get(2)));
+		
+		HttpSession session = request.getSession();
+		
+		session.setAttribute("firstName", firstName);
+		session.setAttribute("lastName", lastName);
+		session.setAttribute("university", university);
+		session.setAttribute("faculty", faculty);
+		session.setAttribute("answers", answers);
+	}
+
+}
