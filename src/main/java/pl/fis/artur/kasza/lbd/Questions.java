c5b38d5a747c48f390cd6afacbef94cd2d6c0c58
Artur Kasza <arturkasza98@gmail.com>
2019-07-12 14:32:57 +0200

    Packages in order. User authentication in progress

--- src/main/java/pl/fis/artur/kasza/lbd/Questions.java
+++
@@ -1,5 +0,0 @@
-package pl.fis.artur.kasza.lbd;
-
-public class Questions {
-	public static String[] questions = {"Quality of courses & teaching?", "Contact with teachers?", "Inclusion of work/practical experience?"};
-}
